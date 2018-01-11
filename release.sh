41f23560f467dd35dec972698475b7f5f913ffab
Andrej Petras <andrej@lorislab.org>
2018-01-11 22:48:38 +0100

    Update for the release

--- release.sh
+++ release.sh
@@ -3,12 +3,9 @@
 export VERSION=0.10-SNAPSHOT
 export RELEASE=$1
 
-
 git clean -f && git reset --hard origin/master
 
-mvn -DtagNameFormat=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${VERSION} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:prepare -B 
+mvn -DtagNameFormat=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${VERSION} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:prepare release:perform -B 
 
-mvn -DtagNameFormat=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${VERSION} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:perform -B 
-
-#git push origin $RELEASE
+git push origin $RELEASE
 
