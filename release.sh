144a39253320253619c4ef01caaa16aa35f0c1c4
Andrej Petras <andrej@lorislab.org>
2018-01-11 22:34:06 +0100

    Update for the release

--- release.sh
+++ release.sh
@@ -6,7 +6,9 @@ export RELEASE=$1
 
 git clean -f && git reset --hard origin/master
 
-mvn -DtagNameFormat=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${VERSION} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -Darguments=-DreleaseVersion=${RELEASE} -DaltDeploymentRepository=bintray::default::https://api.bintray.com/maven/lorislab/maven/bridj release:prepare release:perform -B 
+mvn -DtagNameFormat=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${VERSION} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -Darguments=-DaltDeploymentRepository=bintray::default::https://api.bintray.com/maven/lorislab/maven/bridj release:prepare -B 
+
+mvn -DtagNameFormat=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${VERSION} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -Darguments=-DaltDeploymentRepository=bintray::default::https://api.bintray.com/maven/lorislab/maven/bridj release:perform -B 
 
 git push origin $RELEASE
 
